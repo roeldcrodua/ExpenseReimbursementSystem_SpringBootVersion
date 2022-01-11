9b32f334a3bb2ec92bb0d6962f784d77e6c7c786
Roel Crodua <roelcrodua@yahoo.com>
2022-01-11 16:27:38 -0500

    Added user login functionality in angular web app.

---
+++ angular-front-end/ers-web-app/src/app/components/reimbursement/reimbursement-list/reimbursement-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reimbursement-list',
+  templateUrl: './reimbursement-list.component.html',
+  styleUrls: ['./reimbursement-list.component.css']
+})
+export class ReimbursementListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/main/java/com/revature/ers/repository/ReimbursementDao.java
@@ -0,0 +1,21 @@
+package com.revature.ers.repository;
+
+import com.revature.ers.models.ReimbType;
+import com.revature.ers.models.Reimbursement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Repository("reimbursementDao")
+@Transactional
+public interface ReimbursementDao extends JpaRepository<Reimbursement, Integer> {
+
+    @Query("from Reimbursement where author = :author")
+    List<Reimbursement> findReimbursementByAuthor(Integer author);
+    @Query("from Reimbursement where resolver = :resolverId")
+    List<Reimbursement> findReimbursementByResolver(Integer resolverId);
+}
