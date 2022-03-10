7b401647a12d717c7028305be635de816934ab1f
Roel Crodua <roelcrodua@yahoo.com>
2022-03-10 11:43:47 -0500

    Fix navbar menu display

--- angular-front-end/ers-web-app/src/app/components/navbar/navbar.component.ts
+++ angular-front-end/ers-web-app/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  _user: User = {
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    userId: 0,
+    role: ""
+  };
+  _role: any;
+
+  @Input()
+  _loggedInUser:  User = {
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    userId: 0,
+    role: ""
+  }
+
+  
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.userService.checkSession().subscribe(user => {
+      if (user.success) {
+        this._user = {
+          userId: user.object.userId,
+          username: user.object.username,
+          password: user.object.password,
+          firstName: user.object.firstName,
+          lastName: user.object.lastName,
+          email: user.object.email,
+          role: user.object.role
+        }
+      } else {
+        this.router.navigate([``]);
+      }
+    })  
   }
 
 }
