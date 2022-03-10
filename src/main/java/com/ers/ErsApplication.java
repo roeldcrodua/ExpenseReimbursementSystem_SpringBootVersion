d5133ff3bb7327824a31e7bf389dcd18df69bba1
Roel Crodua <roelcrodua@yahoo.com>
2022-03-10 11:46:39 -0500

    Added Looger functionality

---
+++ src/main/java/com/ers/ErsApplication.java
@@ -0,0 +1,25 @@
+package com.ers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ErsApplication {
+
+	private  static final Logger LOGGER = LogManager.getLogger(ErsApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsApplication.class, args);
+
+		LOGGER.info("Info level log messages");
+		LOGGER.debug("Debug level log messages");
+		LOGGER.warn("Waning level log messages");
+		LOGGER.error("Error level log messages");
+	}
+
+
+}
