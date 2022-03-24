6faf2a701f3c929d4a643dceb51d562dc1d0727c
Roel Crodua <roelcrodua@yahoo.com>
2022-03-24 00:39:36 -0400

    Added functions for reimbursement service to connect to back-end

--- angular-front-end/ers-web-app/src/app/services/reimbursement.service.ts
+++ angular-front-end/ers-web-app/src/app/services/reimbursement.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Reimbursement } from '../model/reimbursement';
+import { GenericService } from './generic.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReimbursementService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private genericService: GenericService) { }
+
+  getAllReimbursement(_loggedInUser: number): Observable<any>{
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement`, {withCredentials: true});
+  }
+
+  getAllOwnReimbursement(_loggedInUser: number): Observable<any>{
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/userId/${_loggedInUser}`, {withCredentials: true});
+  }
+
+  getReimbursementById(_reimbId: number): Observable<any>{
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`, {withCredentials: true});
+  }
+
+  createReimbursement(reimbursement: Reimbursement): Observable<any>{
+    return this.httpClient.post<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {withCredentials: true});
+  }
+
+  editReimbursement(reimbursement: Reimbursement): Observable<any>{
+    return this.httpClient.patch<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {withCredentials: true});
+  }
+
+  deleteReimbursement(_reimbId: number): Observable<any>{
+    return this.httpClient.delete<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`,{withCredentials: true});
+  }
 }
