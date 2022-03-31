cf174c171d1290b754440d81866faadf6b930078
Roel Crodua <roelcrodua@yahoo.com>
2022-03-31 17:16:41 -0400

    Added conditional statements upon updating the changes

--- angular-front-end/ers-web-app/src/app/dialog/confirm/confirm.component.ts
+++ angular-front-end/ers-web-app/src/app/dialog/confirm/confirm.component.ts
@@ -16,6 +16,38 @@ import { UserService } from 'src/app/services/user.service';
 export class ConfirmDialogComponent {
 
   @Input()
+  _isUser: boolean = false;
+  @Input()
+  _type: any;
+  @Input()
+  _description: any;
+  @Input()
+  _amount: any;
+  _action: any;
+  @Input()
+  _status: any;
+  @Input()
+  _isManager: boolean = false;
+  @Input()
+  _isSearchActivated: boolean = false;
+  @Input()
+  _isAddingReimbursement: boolean = false;
+  @Input()
+  _isDeletingReimbursment: boolean = false;
+  @Input()
+  _isEditingReimbursment: boolean = false;
+  @Input()
+  _isRegisteringUser: boolean = false;
+  @Input()
+  _isRemovingUser: boolean = false;
+  @Input()
+  _isUpdatingUser: boolean = false;
+  @Input()
+  _isResetPassword: boolean = false;
+  @Input()
+  _isProcessed: boolean = false;
+  
+  @Input()
   _reimbursementInput: Reimbursement = {
       reimbId: 0,
       amount: 0,
@@ -28,13 +60,6 @@ export class ConfirmDialogComponent {
       status: "",
       type: ""
   }
-  
-  @Input()
-  _type: any;
-  @Input()
-  _description: any;
-  @Input()
-  _amount: any;
 
   _reimbursements: Reimbursement[] = [];
   _reimbursement: Reimbursement = {
@@ -51,28 +76,30 @@ export class ConfirmDialogComponent {
 }
 
 _user: User = {
-  username: "",
+  userName: "",
   password: "",
   firstName: "",
   lastName: "",
   email: "",
   userId: 0,
-  role: ""
+  role: "",
+  picUrl: ""
 };
 
 @Input()
 _specifiedUser:  User = {
-  username: "",
+  userName: "",
   password: "",
   firstName: "",
   lastName: "",
   email: "",
   userId: 0,
-  role: ""
+  role: "",
+  picUrl: ""
 }
 
-  //_dateSubmitted: any = Date.now().toLocaleString();
-  _action: any;
+date: Date = new Date(); 
+_dateResolved: any = this.date.getMonth() + "-" + this.date.getDate() + "-" + this.date.getFullYear();
 
   constructor(private reimbursementService: ReimbursementService,
               private modalService: NgbModal,
@@ -85,28 +112,17 @@ _specifiedUser:  User = {
   ngOnInit(): void {
     this.userService.checkSession().subscribe(user => {
       if (user.success) {
-        if (user.object.role == "EMPLOYEE"){
           this._user = {
             userId: user.object.userId,
-            username: user.object.username,
+            userName: user.object.userName,
             password: user.object.password,
             firstName: user.object.firstName,
             lastName: user.object.lastName,
             email: user.object.email,
-            role: user.object.role
-          }
-        } else { // FOR MANAGER
-          this._user = {
-            userId: this._specifiedUser.userId,
-            username: this._specifiedUser.username,
-            password: this._specifiedUser.password,
-            firstName: this._specifiedUser.firstName,
-            lastName: this._specifiedUser.lastName,
-            email: this._specifiedUser.email,
-            role: this._specifiedUser.role
-          }
+            role: user.object.role,
+            picUrl: user.object.picUrl
+          }        
         }
-      } 
     }) 
   }
 
@@ -126,8 +142,29 @@ _specifiedUser:  User = {
     this.modalService.open(reimbursement);
   }
 
+  resolveReimbursement(reimbursement: any){
+    this._action = "resolve";
+    this.modalService.open(reimbursement);
+  }
+
+  addNewUser(user: any){
+    this._action = "register";
+    this.modalService.open(user);
+  }
+
+  deleteUser(user: any){
+    this._action = "remove";
+    this.modalService.open(user);
+  }
+
+  editUser(user: any){
+    this._action = "update";
+    this.modalService.open(user);
+  }
+
 
   updateChanges(){
+      ////////////////////////////////////
       if (this._action == "add"){
         this._reimbursement.type = this._type;
         this._reimbursement.description = this._description;
@@ -140,18 +177,24 @@ _specifiedUser:  User = {
             this.modalService.dismissAll();
             this.reimbursementList.hasChanges=true; 
             this.reimbursementList.ngOnChanges();   
+            alert(data.message);
           }
         })
-      } else if (this._action  == "delete"){
+      } 
+      ////////////////////////////////////
+      else if (this._action  == "delete"){
         this._reimbursement = this._reimbursementInput;
         this.reimbursementService.deleteReimbursement(this._reimbursement.reimbId).subscribe(data => {
           if (data.success){
             this.modalService.dismissAll();   
             this.reimbursementList.hasChanges=true; 
-            this.reimbursementList.ngOnChanges();     
+            this.reimbursementList.ngOnChanges();    
+            alert(data.message); 
           }
         })
-      } else if (this._action  == "edit"){
+      } 
+      ////////////////////////////////////
+      else if (this._action  == "edit"){
         this._reimbursement = this._reimbursementInput;
         this._reimbursement.type = this._type;
         this._reimbursement.description = this._description;
@@ -161,9 +204,68 @@ _specifiedUser:  User = {
           if (data.success){
             console.log(data);
             this.modalService.dismissAll();
-
+            this.reimbursementList.hasChanges=true; 
+            this.reimbursementList.ngOnChanges();  
+            alert(data.message);
           }
         })
       } 
+      ////////////////////////////////////
+      else if (this._action == "resolve"){
+        console.log("INPUT REIMBURSEMENT:")
+        console.log(this._reimbursementInput)
+        this._reimbursement = this._reimbursementInput;
+        this._reimbursement.status = this._status;
+        this._reimbursement.dateResolved = this._dateResolved;
+        this._reimbursement.resolver = this._user.userId;
+        console.log("OUTPUT REIMBURSEMENT:")
+        console.log(this._reimbursement)
+        this.reimbursementService.editReimbursement(this._reimbursement).subscribe(data => {
+          if (data.success){
+            console.log(data);
+            this.modalService.dismissAll();
+            this.reimbursementList.hasChanges=true; 
+            this.reimbursementList.ngOnChanges();  
+            alert(data.message);
+          }
+        })
+      } 
+      ////////////////////////////////////
+      else if (this._action  == "register"){
+        console.log("REGISTER");
+        console.log(this._specifiedUser);
+        this.userService.createUser(this._specifiedUser).subscribe( data => {
+          if (data.success){
+            console.log(data.object)
+            this.modalService.dismissAll();
+            alert(data.message);
+          }
+        })
+      } 
+      ////////////////////////////////////
+      else if (this._action  == "remove"){
+        console.log("REMOVE");
+        console.log(this._specifiedUser);
+        alert("This is not yet implemented!");
+        // this.userService.deleteUser(this._specifiedUser.userName).subscribe( data => {
+        //   if (data.success){
+        //     console.log(data.object)
+        //     this.modalService.dismissAll();
+        //   }
+        //})
+      } 
+      ////////////////////////////////////
+      else if (this._action  == "update"){
+        console.log("UPDATE");
+        console.log(this._specifiedUser);
+        this.userService.editProfile(this._specifiedUser).subscribe( data => {
+          if (data.success){
+            console.log("OBJECT");
+            console.log(data.object)
+            this.modalService.dismissAll();
+            alert(data.message);
+          }
+        })
+      }
   }
 }
