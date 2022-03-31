8a41200c79655166d674c20a25675914be7ff31b
Roel Crodua <roelcrodua@yahoo.com>
2022-03-31 17:06:54 -0400

    Remove unnecessary component

--- angular-front-end/ers-web-app/src/app/app-routing.module.ts
+++ angular-front-end/ers-web-app/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { RegisterComponent } from './components/user/register/register.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'login', component: LoginComponent}
 ];
--- angular-front-end/ers-web-app/src/app/app.module.ts
+++ angular-front-end/ers-web-app/src/app/app.module.ts
@@ -4,10 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { RegisterComponent } from './components/user/register/register.component';
 import { ForgotPasswordComponent } from './components/user/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/user/reset-password/reset-password.component';
-import { EditProfileComponent } from './components/user/edit-profile/edit-profile.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -17,22 +15,22 @@ import { ReimbursementListComponent } from './components/reimbursement/reimburse
 import { OrderbyPipe } from './pipe/orderby.pipe';
 import { ViewModalComponent } from './components/reimbursement/view-reimbursement/view-reimbursement.component';
 import { ConfirmDialogComponent } from './dialog/confirm/confirm.component';
+import { UserInfoComponent } from './components/user/user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     ForgotPasswordComponent,
     ResetPasswordComponent,
-    EditProfileComponent,
     UserProfileComponent,
     DashboardComponent,
     NavbarComponent,
     ReimbursementListComponent,
     OrderbyPipe,
     ViewModalComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
+    UserInfoComponent
 
   ],
   imports: [
@@ -42,7 +40,7 @@ import { ConfirmDialogComponent } from './dialog/confirm/confirm.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [ReimbursementListComponent],
+  providers: [ReimbursementListComponent, ViewModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
