2dbc8e2bf35bb12a614c59e810ba085a3e72fd5b
Roel Crodua <roelcrodua@yahoo.com>
2022-05-17 15:37:54 -0400

    Added JWT session management and added to Header as values

--- angular-front-end/ers-web-app/src/app/services/reimbursement.service.ts
+++ angular-front-end/ers-web-app/src/app/services/reimbursement.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Reimbursement } from '../model/reimbursement';
@@ -9,37 +9,56 @@ import { GenericService } from './generic.service';
 })
 export class ReimbursementService {
 
+  jwtToken = (sessionStorage.getItem('JWT')!);
+
+  headers = new HttpHeaders().set('content-type', 'application/json')
+                             .set('Access-Control-Allow-Origin', '*')
+                             .set('Authorization', this.jwtToken);
+
+  setHeaders(): void {
+    this.headers = new HttpHeaders().set('content-type', 'application/json')
+                             .set('Access-Control-Allow-Origin', '*')
+                             .set('Authorization', this.jwtToken);
+  }
   constructor(private httpClient: HttpClient, private genericService: GenericService) { }
 
   getAllReimbursement(): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement`, {'headers': this.headers});
   }
 
   getAllReimbursementByStatus(statusId: number): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/status/${statusId}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/status/${statusId}`, {'headers': this.headers});
   }
 
   getAllOwnReimbursement(_loggedInUser: number): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/userId/${_loggedInUser}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/userId/${_loggedInUser}`, {'headers': this.headers});
   }
 
   getAllReimbursementByResolver(): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/resolved`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/resolved`, {'headers': this.headers});
   }
 
   getReimbursementById(_reimbId: number): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`, {'headers': this.headers});
   }
 
   createReimbursement(reimbursement: Reimbursement): Observable<any>{
-    return this.httpClient.post<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.post<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {'headers': this.headers});
   }
 
   editReimbursement(reimbursement: Reimbursement): Observable<any>{
-    return this.httpClient.patch<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.patch<any>(this.genericService._localServerDomain + `/reimbursement`, reimbursement, {'headers': this.headers});
   }
 
   deleteReimbursement(_reimbId: number): Observable<any>{
-    return this.httpClient.delete<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`,{withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.delete<any>(this.genericService._localServerDomain + `/reimbursement/${_reimbId}`, {'headers': this.headers});
   }
 }
--- angular-front-end/ers-web-app/src/app/services/user.service.ts
+++ angular-front-end/ers-web-app/src/app/services/user.service.ts
@@ -1,66 +1,106 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GenericService } from './generic.service';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClient: HttpClient, private genericService: GenericService) {
+  jwtToken = (sessionStorage.getItem('JWT')!);
 
-   }
+  headers = new HttpHeaders().set('content-type', 'application/json')
+                            .set('Access-Control-Allow-Origin', '*')
+                            .set('authorization', this.jwtToken);
+
+  setHeaders(): void {
+    this.jwtToken = (sessionStorage.getItem('JWT')!);
+    this.headers = new HttpHeaders().set('content-type', 'application/json')
+                             .set('Access-Control-Allow-Origin', '*')
+                             .set('authorization', this.jwtToken);
+
+  }
+
+  getDecodedAccessToken(token: string): void {
+    try {
+      let user = jwt_decode(token);
+      sessionStorage.setItem('userObj', JSON.stringify(user));
+      sessionStorage.setItem('JWT', token);
+    } catch(Error) {
+    }
+  }
+
+  isValidToken(token: any): boolean {
+    try {
+      if (Date.now().toString().substring(0,10) < token){
+        return true;
+      }
+      return false;
+    } catch(Error) {
+        return false;
+    }
+  }
+
+  constructor(private httpClient: HttpClient, private genericService: GenericService, private router: Router) {
+
+  }
 
   createUser(newUser: User) : Observable<any>{
-    return this.httpClient.post<any>(this.genericService._localServerDomain + "/user", newUser, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.post<any>(this.genericService._localServerDomain + "/user", newUser, {'headers': this.headers});
   }
 
   editProfile(existingUser: User): Observable<any>{
-    return this.httpClient.patch<any>(this.genericService._localServerDomain  + "/user", existingUser, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.patch<any>(this.genericService._localServerDomain  + "/user", existingUser, {'headers': this.headers});
   }
 
   deleteUser(userInput: String): Observable<any>{
-    return this.httpClient.delete<any>(this.genericService._localServerDomain  + `/user/${userInput}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.delete<any>(this.genericService._localServerDomain  + `/user/${userInput}`, {'headers': this.headers});
   }
 
-  getListOfUser(): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain + "/user", {withCredentials: true});
+  verifyUser(userInput: any): Observable<any>{
+    return this.httpClient.post<any>(this.genericService._localServerDomain + "/user/verify", userInput);
   }
 
   getUserByEmail(email: String): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/email/${email}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/email/${email}`, {'headers': this.headers});
   }
 
   getUserByuserName(userName: String): Observable<any> {
-
-    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/username/${userName}`, {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/username/${userName}`, {'headers': this.headers});
   }
 
   getUserById(id: any): Observable<any> {
-
-    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/${id}`, {withCredentials: true});
-  }
-
-  checkSession(): Observable<any>{
-    return this.httpClient.get<any>(this.genericService._localServerDomain  + "/check-session", {withCredentials: true});
+    this.setHeaders();
+    return this.httpClient.get<any>(this.genericService._localServerDomain  + `/user/${id}`, {'headers': this.headers});
   }
 
   userLogin(userName: string, password: string): Observable<any>{
     return this.httpClient.post<any>(this.genericService._localServerDomain  + "/login",{
       userName: userName,
       password: password
-    }, {withCredentials: true});
+    });
   }
 
   logout(){
-    return this.httpClient.get<any>(this.genericService._localServerDomain  + "/logout", {withCredentials: true});
+    sessionStorage.clear();
+    localStorage.clear();
+    this.headers = new HttpHeaders();
+    this.router.navigateByUrl('');
   }
 
   forgotPassword(email: string): Observable<any>{
-    return this.httpClient.patch<any>(this.genericService._localServerDomain  + `/forgot-password/${email}`, {withCredentials: true});
+    this.setHeaders();
+    console.log("FORGOT PASSWORD")
+    return this.httpClient.patch<any>(this.genericService._localServerDomain  + `/forgot-password/${email}`, {'headers': this.headers});
   }
-
 
 }
