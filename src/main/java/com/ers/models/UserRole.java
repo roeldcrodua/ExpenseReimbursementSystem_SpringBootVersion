3a6d7d22b942ca1bd0b7977ffabf5954c09c26b5
Roel Crodua <roelcrodua@yahoo.com>
2022-05-17 15:31:55 -0400

    Change the UserRole value

--- src/main/java/com/ers/models/UserRole.java
+++ src/main/java/com/ers/models/UserRole.java
@@ -1,17 +1,14 @@
 package com.ers.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
 public enum UserRole {
-    EMPLOYEE(0),
-    MANAGER(1),
-    ADMIN(2);
-
-    private int roleId;
-
+    EMPLOYEE,
+    MANAGER,
+    ADMIN
 }
