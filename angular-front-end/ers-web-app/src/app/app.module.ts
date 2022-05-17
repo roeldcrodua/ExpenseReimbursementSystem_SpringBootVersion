82560d0f1b9a6294466e698b039e83349e2799d0
Roel Crodua <roelcrodua@yahoo.com>
2022-05-17 15:39:49 -0400

    Remove the unecessary links

--- angular-front-end/ers-web-app/src/app/app.module.ts
+++ angular-front-end/ers-web-app/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { ForgotPasswordComponent } from './components/user/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './components/user/reset-password/reset-password.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -21,8 +19,6 @@ import { UserInfoComponent } from './components/user/user-info/user-info.compone
   declarations: [
     AppComponent,
     LoginComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
     UserProfileComponent,
     DashboardComponent,
     NavbarComponent,
